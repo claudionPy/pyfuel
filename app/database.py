aaa5edfb3f27580b527e0ee47d5cee904e156e93
claudionPy <lopreiatoclaudio.projects@gmail.com>
2025-06-06 18:10:21 +0200

    first dockerized version of PyFuel
    Testing the new version of pyfuel which includes docker to containerize the application

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:adm69252892@localhost:5432/fuel_db"
+DATABASE_URL = os.getenv("DB_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
